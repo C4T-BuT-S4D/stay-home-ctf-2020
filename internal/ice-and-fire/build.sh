3b395ba62b503bc9153376b8b15ed73975459e76
Roman Nikitin <nikrom2012@me.com>
2020-03-26 02:18:37 +0300

    Better internal ice-and-fire image

--- internal/ice-and-fire/build.sh
+++ internal/ice-and-fire/build.sh
@@ -4,9 +4,9 @@ set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 echo "[*] Building the image"
-docker build --no-cache -t fl3x3dd/ice-and-fire-base:latest "${DIR}"
+docker build -t pomomondreganto/ice-and-fire-base:latest "${DIR}"
 
 echo "[*] Pushing the image"
-docker push fl3x3dd/ice-and-fire-base:latest
+docker push pomomondreganto/ice-and-fire-base:latest
 
 echo "[+] Done!"
