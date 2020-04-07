189970fc16d47176305d8f63911255d3f6f735c4
Roman Nikitin <nikrom2012@me.com>
2020-04-07 14:14:26 +0300

    Added front base

--- internal/ice-and-fire/front/src/views/Index.vue
+++ internal/ice-and-fire/front/src/views/Index.vue
@@ -1,5 +1,14 @@
 <template>
-    <div>
-        kek
-    </div>
+    <layout>
+        <p>LOL</p>
+    </layout>
 </template>
+
+<script>
+import Layout from '@/layouts/Layout';
+export default {
+    components: {
+        Layout
+    }
+};
+</script>
