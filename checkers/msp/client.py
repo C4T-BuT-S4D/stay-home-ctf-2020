d9d75c7a33a601b3f346fb7ca48caef1af7df4ba
Alexey Kharlamov <der@2-47.ru>
2020-04-12 13:44:55 +0200

    msp: fix checker error message

--- checkers/msp/client.py
+++ checkers/msp/client.py
@@ -27,7 +27,7 @@ class CheckMachine:
 
         if 'error' in response_body or 'object' not in response_body:
             self.c.cquit(Status.MUMBLE, "GROUND TERMINAL ERROR",
-                         '/telemetry/ error: ' + response['error'])
+                         '/telemetry/ error: ' + response.get('error', 'none'))
 
         return response_body['object']
 
